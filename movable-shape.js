09cc103bcdff6b57e051ceb395aa393545d1bfcd
Joe Torok <fm7222@wayne.edu>
2016-02-01 20:08:14 -0500

    a little slower acceleration

--- movable-shape.js
+++ movable-shape.js
@@ -33,7 +33,7 @@ var movableShape = {
 	// If you wanted to build some acceleration in here, you might add a line or two to modify the speed
 	update: function () {
 		this.move();
-		this.speed.y *= 1.05;
+		this.speed.y *= 1.07;
 
 	},
 
